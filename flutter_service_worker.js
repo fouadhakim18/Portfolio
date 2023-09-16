a12127f663a6fa2427e942b0f1498e7a8a6dad37
Hakim Maroc <fouadmaroc625@gmail.com>
2023-09-16 18:55:07 +0100

    Deploy

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -58,8 +58,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "0ea7b52f93f765f227de045b6aebefcb",
-"/": "0ea7b52f93f765f227de045b6aebefcb",
+"index.html": "0f3831c97e1aa184b36c83da9390e4ae",
+"/": "0f3831c97e1aa184b36c83da9390e4ae",
 "main.dart.js": "614af0f5aa3e986a221255ff0048e772",
 "manifest.json": "b58fcfa7628c9205cb11a1b2c3e8f99a",
 "version.json": "d351ae99551ba008177d734c54348c46"
